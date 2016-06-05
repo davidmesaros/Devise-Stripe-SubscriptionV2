efd44f183ebe44f612873827cc2c95a9a0f71b84
davidmesaros <david.mesaros@hotmail.com>
2016-06-05 22:29:39 +1000

    schema bugs

--- db/migrate/20160531040157_remove_columns_from_users.rb
+++ db/migrate/20160531040157_remove_columns_from_users.rb
@@ -1,7 +1,7 @@
 class RemoveColumnsFromUsers < ActiveRecord::Migration
   def change
-    # remove_column :users, :subscribed, :string
-    remove_column :users, :boolean, :string
+    remove_column :users, :subscribed, :string
+    # remove_column :users, :boolean, :string
     remove_column :users, :stripeid, :string
     remove_column :users, :end_date, :date
     remove_column :users, :delinquent, :boolean
