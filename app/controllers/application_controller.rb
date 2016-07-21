32e67015c4407003ef950eaf3b926e82a722fea2
davidmesaros <david.mesaros@hotmail.com>
2016-07-21 22:44:47 +1000

    update average

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -191,7 +191,6 @@ class ApplicationController < ActionController::Base
                   if value_floated.to_s[".5"] and x = 0
                     @average << value_floated.floor if !value_floated.nan?
                     x = 1
-                    byebug
                   else
                     @average << value_floated.round(0) if !value_floated.nan?
                   end
