93930ebd653cd0a207582b6f9cb40fcd09cfd235
davidmesaros <david.mesaros@hotmail.com>
2016-07-29 11:32:15 +1000

    canx update mail to dm

--- app/mailers/swiftads_mailer.rb
+++ app/mailers/swiftads_mailer.rb
@@ -2,7 +2,7 @@ class SwiftadsMailer < ApplicationMailer
   def swiftads_email(user, website)
     @user = user
     @website = website
-    mail(to: @user.email, bcc: 'swiftads.au@gmail.com', subject: 'Swiftads Email Notification')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Swiftads Email Notification')
   end
 
   def swiftads_email_update(user, website, product, product1, product2)
@@ -11,21 +11,21 @@ class SwiftadsMailer < ApplicationMailer
     @product = product
     @product1 = product1
     @product2 = product2
-    mail(to: @user.email, bcc: 'swiftads.au@gmail.com', subject: 'Swiftads Email Notification')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Swiftads Email Notification')
   end
 
   def cancel_subscription(user, website, stripeid)
     @user = user
     @website = website
     @stripeid = stripeid
-    mail(to: @user.email, bcc: 'david.mesaros@swiftads.com.au', subject: 'Swiftads Cancellation Email Notification')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Swiftads Cancellation Email Notification')
   end 
 
   def new_subscription(user, website, stripeid)
     @user = user
     @website = website
     @stripeid = stripeid
-    mail(to: @user.email, bcc: 'david.mesaros@swiftads.com.au', subject: 'Confirmation of Payment - Swiftads')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Confirmation of Payment - Swiftads')
     
   end
 
@@ -33,7 +33,7 @@ class SwiftadsMailer < ApplicationMailer
     @user = user
     @website = website
     @advertise = description
-    mail(to: @user.email, bcc: 'swiftads.au@gmail.com', subject: 'Google Ad for your Website is Now Ready')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Google Ad for your Website is Now Ready')
     
   end
 
@@ -41,19 +41,19 @@ class SwiftadsMailer < ApplicationMailer
     @user = user
     @website = website
     @stripeid = stripeid
-    mail(to: @user.email, bcc: 'swiftads.au@gmail.com', subject: 'Tax Invoice - Swiftads')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Tax Invoice - Swiftads')
   end
 
   def dashboard_update(user, website)
     @user = user
     @website = website
-    mail(to: @user.email, bcc: 'swiftads.au@gmail.com', subject: 'Swiftads Dashboard Update')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Swiftads Dashboard Update')
   end
 
   def last_dashboard_update(user, website)
     @user = user
     @website = website
-    mail(to: @user.email, bcc: 'swiftads.au@gmail.com', subject: 'Swiftads Dashboard Update')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Swiftads Dashboard Update')
   end
 
   def swiftads_invoice_refund(user, website, stripeid, balance)
@@ -61,7 +61,7 @@ class SwiftadsMailer < ApplicationMailer
     @website = website
     @stripeid = stripeid
     @balance = balance
-    mail(to: @user.email, bcc: 'david.mesaros@swiftads.com.au', subject: 'Tax Invoice - Refund - Swiftads')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Tax Invoice - Refund - Swiftads')
   end
 
    def swiftads_invoice_cancel_refund(user, website, stripeid, balance)
@@ -69,14 +69,14 @@ class SwiftadsMailer < ApplicationMailer
     @website = website
     @stripeid = stripeid
     @balance = balance
-    mail(to: @user.email, bcc: 'david.mesaros@swiftads.com.au', subject: 'Tax Invoice - Refund - Swiftads')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Tax Invoice - Refund - Swiftads')
   end
 
   def trial_period(user, website, stripeid)
     @user = user
     @website = website
     @stripeid = stripeid
-    mail(to: @user.email, bcc: 'swiftads.au@gmail.com', subject: 'Trail Period - Swiftads')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Trail Period - Swiftads')
   end
 
   def dashboard_update_stats(user, website, calls, clicks, searches, cost, balance, smartphones, tablets, computers )
@@ -90,6 +90,6 @@ class SwiftadsMailer < ApplicationMailer
     @smartphones = smartphones
     @tablets = tablets
     @computers = computers
-    mail(to: @user.email, bcc: 'swiftads.au@gmail.com', subject: 'Daily Dashboard Update - Swiftads')
+    mail(to: @user.email, bcc: 'info@swiftads.com.au', subject: 'Daily Dashboard Update - Swiftads')
   end
 end
