f67918ac1bde697691a7039a7834de9d30c3b275
davidmesaros <david.mesaros@hotmail.com>
2016-09-05 21:54:08 +1000

    updated mailer

--- Gemfile
+++ Gemfile
@@ -15,6 +15,8 @@ gem 'bootstrap-will_paginate', '0.0.10'
 
 gem "browser"
 
+gem "payday"
+
 
 # Use sqlite3 as the database for Active Record
 # gem 'sqlite3'
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
   resources :advertises
   resources :subscribers
   resources :cancel_plans
+  resources :invoices
+  resources :line_items
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
